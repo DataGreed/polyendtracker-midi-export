f251332118527284c18eb87703bec08f8b08ff13
DataGreed <cpr.al.exe@gmail.com>
2022-03-31 19:27:35 +0100

    project parser stub

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 
 if __name__ == '__main__':
 
-    from parsers import patterns
+    from parsers import patterns, project
 
-    # todo: remove this and implement tests
-    p = patterns.PatternParser(
-        # NOTE: this file was created with firmware 1.3.1 or older version
-        filename="./reverse-engineering/session 1/project files/datagreed - rebel path tribute 2/patterns/pattern_06.mtp")
-    parsed_pattern = p.parse()
-    # print(parsed_pattern.render_as_table())
-    from exporters import midi
+    # # todo: remove this and implement tests
+    # p = patterns.PatternParser(
+    #     # NOTE: this file was created with firmware 1.3.1 or older version
+    #     filename="./reverse-engineering/session 1/project files/datagreed - rebel path tribute 2/patterns/pattern_06.mtp")
+    # parsed_pattern = p.parse()
+    # # print(parsed_pattern.render_as_table())
+    # from exporters import midi
+    #
+    # midi_exporter = midi.PatternToMidiExporter(pattern=parsed_pattern)
+    # print(midi_exporter.generate_midi())
+    # midi_exporter.write_midi_file("./test_midi_file.mid")
 
-    midi_exporter = midi.PatternToMidiExporter(pattern=parsed_pattern)
-    print(midi_exporter.generate_midi())
-    midi_exporter.write_midi_file("./test_midi_file.mid")
+
+    project = project.ProjectParser(
+        filename_or_folder="./reverse-engineering/session 1/project files/datagreed - rebel path tribute 2/"
+    )
+
+    parsed_project = project.parse()
+    print("Finished parsing project.")
+    print(f"BPM: {parsed_project.bpm}")
 
 
---
+++ parsers/project.py
@@ -0,0 +1,130 @@
+# polyend tracker project file
+
+__author__ = "Alexey 'DataGreed' Strelkov"
+
+import os
+from typing import List, Pattern, Dict
+
+
+class Song:
+    """
+    Represents a song, a sequence of patterns
+    """
+
+    """Maximum length of a song in patterns."""
+    MAXIMUM_SLOTS_PER_SONG = 255 # from docs
+
+    def __init__(self, pattern_chain: List[int], pattern_mapping: Dict[int:Pattern]):
+        """
+
+        :param pattern_chain: a list of ints representing the order of patterns in the song.
+         ints a 1-based. So pattern 1 is represented by 1.
+        :param pattern_mapping:   a dict with all of the unique patterns in the song.
+        Keys are integer number of patterns, values are actual patterns.
+        """
+        if len(pattern_chain)>self.MAXIMUM_SLOTS_PER_SONG:
+            raise ValueError(f"Maximum song length is {self.MAXIMUM_SLOTS_PER_SONG}, "
+                             f"but {len(pattern_chain)} pattern long song received")
+        self.pattern_mapping = pattern_mapping
+        self.pattern_chain = pattern_chain
+
+    def get_song_as_patterns(self) -> List[Pattern]:
+        """Returns song as a list of patterns ordered. Play them in
+        the returned order to get the song"""
+        return [self.pattern_mapping[x] for x in self.pattern_chain]
+
+
+class Project:
+    """
+    Represents a tracker project
+    """
+
+    def __init__(self, name: str, song: Song, bpm: int):
+        """
+        :param name: name of the project
+        :param bpm: project tempo in beats per minutes. Tracker supports fractional tempo
+        :param song: song (sequence of patterns to play)
+        """
+        self.bpm = bpm
+        self.name = name
+        self.song = song
+
+
+    OFFSET_END = 0x624 #1572 bytes total files length
+    OFFSET_START = 0
+
+    @staticmethod
+    def from_bytes(data: bytes, patterns_bytes=Dict[int:bytes]) -> "Project":
+        """
+        Constructs a project object from bytes extracted from project file.
+        :param data:
+        :param patterns_bytes: a list of bytes for each of projects
+        patterns extracted from pattern files.
+        :return:
+        """
+        expected_length = Project.OFFSET_END - Project.OFFSET_START  # 6152 or 769*8 just for sanity check
+
+        if len(data) != expected_length:
+            raise ValueError(f"Expected project data {expected_length} bytes long, got {len(data)} instead")
+
+        raise NotImplementedError()
+
+        return Project()
+
+
+class ProjectParser:
+    """Parses tracker *.mt files, scans directory for patterns
+    so a song can be exported to MIDI"""
+
+    PATTERNS_FOLDER_NAME = "patterns"
+    DEFAULT_PROJECT_FILENAME = "project.mt"
+    PATTERN_FILE_NAME_TEMPLATE = "pattern_{}.mtp"   # todo: format: add leading zero automatically
+
+    MAXIMUM_PATTERNS_PER_PROJECT = 255  # from polyend docs
+
+    def __init__(self, filename_or_folder: str):
+        """
+        :param filename_or_folder: project (*.mt) filename or folder with project file.
+        Note that pattern files are required to be in a "patterns" subfolder within
+        the same folder for everything to work properly.
+        """
+        if filename_or_folder.endswith(".mt"):
+            self.filepath = filename_or_folder
+            # folder must always end with a folder separator
+            self.folder = os.sep.join(filename_or_folder.split(os.sep)[:-1]) + os.sep
+        else:
+            self.folder = filename_or_folder
+            if not self.folder.endswith(os.sep):
+                # folder must always end with a folder separator
+                self.folder += os.sep
+            self.filepath = self.folder+self.DEFAULT_PROJECT_FILENAME
+
+        self.patterns_folder = self.folder + self.PATTERNS_FOLDER_NAME + os.sep
+
+    def parse(self) -> Project:
+
+        project_file_bytes = None
+        pattern_file_bytes_dict: Dict[int:bytes] = {}
+
+        with open(self.filepath, "rb") as f:
+            project_file_bytes = f.read()  # f.read()[Project.OFFSET_START:Project.OFFSET_END]
+
+        # find all project pattern files and add their bytes to the parser too
+        for i in range(1, self.MAXIMUM_PATTERNS_PER_PROJECT+1):
+            pattern_file_path = self.patterns_folder+self.PATTERN_FILE_NAME_TEMPLATE
+
+            # FIXME: patterns names goe as pattern_01.mtp, but manual says there can be
+            #  255 patterns, how pattern 100 file will be named if
+            #  there is only 1 leading zero in one digit projects?
+
+            pattern_number_string = str(i)
+            if len(pattern_number_string) < 2:
+                pattern_number_string = "0" + pattern_number_string
+
+            pattern_file_path = pattern_file_path.replace("{}", pattern_number_string)
+
+            with open(pattern_file_path, "rb") as f:
+                # pattern
+                pattern_file_bytes_dict[i] = f.read()   # reads the whole file
+
+        return Project.from_bytes(project_file_bytes, pattern_file_bytes_dict)
