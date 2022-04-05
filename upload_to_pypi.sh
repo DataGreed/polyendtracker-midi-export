23b5ec4ae935673c023f8103dfbc01d7a4ff9027
DataGreed <cpr.al.exe@gmail.com>
2022-04-05 17:49:31 +0100

    Update upload_to_pypi.sh

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,4 +1,6 @@
+deactivate
+rm -rf dist/*
 python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 python3 -m pip install --user --upgrade twine
-python3 -m twine upload --repository --verbose pypi dist/*
+python3 -m twine upload --repository pypi dist/* --verbose
